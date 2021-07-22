ec565eccd6dd322bde18d8414082553b46c779c7
Logic <Logic@study.com>
2021-07-22 21:21:19 +0800

    Git—IDEA测试提交

---
+++ src/main/java/gitTest/GitTest.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我的Git项目已经和IDEA连通了");
+    }
+}
+/**
+ * @program: Git-Demo
+ * @description: Git测试
+ * @author: Administrator
+ * @create: 2021-07-22 21:02
+ **/
