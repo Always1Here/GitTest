4c8881d3c319461f5b6c35ed784b7777c0375db0
Logic <Logic@study.com>
2021-07-22 22:01:46 +0800

    Logic commit2

--- src/main/java/gitTest/GitTest.java
+++ src/main/java/gitTest/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         int i=1;
         System.out.println("我的Git项目已经和IDEA连通了");
+        System.out.println("主线");
         System.out.println("Logic分支");
     }
 }
