c5a65ca65f65206ae2b247b40d141b820198932d
Logic <Logic@study.com>
2021-07-22 21:22:24 +0800

    Git—IDEA测试提交

--- src/main/java/gitTest/GitTest.java
+++ src/main/java/gitTest/GitTest.java
@@ -1,13 +1,14 @@
 package gitTest;
 
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("我的Git项目已经和IDEA连通了");
-    }
-}
 /**
  * @program: Git-Demo
  * @description: Git测试
  * @author: Administrator
  * @create: 2021-07-22 21:02
  **/
+public class GitTest {
+    public static void main(String[] args) {
+        int i=1;
+        System.out.println("我的Git项目已经和IDEA连通了");
+    }
+}
